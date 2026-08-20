099b80f1b119db1dae12fb39e508b0281758c23e
gourabsaha1992 <contactgourabsaha@gmail.com>
2026-08-20 15:33:03 +0530

    Update relic_ep_param.c

--- Relic_updated/src/ep/relic_ep_param.c
+++ Relic_updated/src/ep/relic_ep_param.c
@@ -29,7 +29,7 @@
  * @ingroup ep
  */
 
-/* In the updated version of Relic, the parameters for BLS24-559 and BLS48-571
+/* In the updated version of Relic, the parameters for STARK curve, BLS24-559 and BLS48-571
  * have been added, along with an alternative parametrization for BLS24-509
  * and BLS48-575. Isogeny maps for curves other than BLS12-381 have also
  * been added.
